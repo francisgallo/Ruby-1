dba4eb679067f5fd0d50b0b03c46bd4107f8a1e2
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-22 13:39:16 -0400

    last line

--- exercise1.rb
+++ exercise1.rb
@@ -2,3 +2,11 @@ puts 2+3
 puts 2
 puts 3
 puts 2+3
+puts 'hello world'
+puts "hello world"
+puts "betty's pie shop"
+puts "Ada Lovelace lived for #{1852-1815} years"
+puts "hello\t\tworld"
+puts "hello\b\b\b\bGoodbye world"
+puts "hello\rStart over world"
+puts "1. hello\n2. World"
