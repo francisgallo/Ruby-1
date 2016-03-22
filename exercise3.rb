2b5044dd531de757f15139a057920fca305dcbdb
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-22 14:13:45 -0400

    Added calc for year born

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
  puts "what is your name?"
 name=gets.chomp
 puts "Hi #{name} it is nice to meet you"
+puts "how old are you?"
+age= gets.chomp.to_i
+year=2016
+puts "you were born in the year #{year - age}"
